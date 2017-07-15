23755860805b4062d76017d39b51db2a6dfbe436
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-15 23:20:04 +0300

    Added Session Constants

--- app/config/config.php
+++ app/config/config.php
@@ -12,6 +12,7 @@ define('LANGUAGES_PATH', APP_PATH . DS . 'languages' . DS);
 define('CSS', '/css/');
 define('JS', '/js/');
 
+// Database Credentials
 defined('DATABASE_HOST_NAME')       ? null : define ('DATABASE_HOST_NAME', 'localhost');
 defined('DATABASE_USER_NAME')       ? null : define ('DATABASE_USER_NAME', 'root');
 defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', 'rebo');
@@ -19,4 +20,10 @@ defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'mvc');
 defined('DATABASE_PORT_NUMBER')     ? null : define ('DATABASE_PORT_NUMBER', 3306);
 defined('DATABASE_CONN_DRIVER')     ? null : define ('DATABASE_CONN_DRIVER', 1);
 
+// Default application language
 defined('APP_DEFAULT_LANGUAGE')     ? null : define ('APP_DEFAULT_LANGUAGE', 'ar');
+
+// Session Information
+define('SESSION_NAME', '_STORE_SESSION');
+define('SESSION_LIFE_TIME', 0);
+define('SESSION_SAVE_PATH', APP_PATH . DS . '..' . DS . 'sessions');
