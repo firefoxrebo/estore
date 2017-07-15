7cf494c7ea3d4ff495a31a138be28ca7eb3f3097
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-16 01:54:46 +0300

    Added a call to index.default language file

--- app/controllers/indexcontroller.php
+++ app/controllers/indexcontroller.php
@@ -6,6 +6,7 @@ class IndexController extends AbstractController
     public function defaultAction()
     {
         $this->_language->load('template.common');
+        $this->_language->load('index.default');
         $this->_view();
     }
 
