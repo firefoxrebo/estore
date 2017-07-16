a40b9300284a5e2e937120081506c525ef1d4efb
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-16 03:19:09 +0300

    modified the default action view file to list the users

--- app/views/users/default.view.php
+++ app/views/users/default.view.php
@@ -1,1 +1,30 @@
 <h1><?= $text_header ?></h1>
+<div class="container">
+    <a href="/users/create" class="button"><i class="fa fa-plus"></i> <?= $text_new_item ?></a>
+    <table class="data">
+        <thead>
+            <tr>
+                <th><?= $text_table_username ?></th>
+                <th><?= $text_table_group ?></th>
+                <th><?= $text_table_email ?></th>
+                <th><?= $text_table_subscription_date ?></th>
+                <th><?= $text_table_last_login ?></th>
+                <th><?= $text_table_control ?></th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php if(false !== $users): foreach ($users as $user): ?>
+            <tr>
+                <td><?= $user->Username ?></td>
+                <td><?= $user->GroupName ?></td>
+                <td><?= $user->Email ?></td>
+                <td><?= $user->SubscriptionDate ?></td>
+                <td><?= $user->LastLogin ?></td>
+                <td>
+
+                </td>
+            </tr>
+        <?php endforeach; endif; ?>
+        </tbody>
+    </table>
+</div>
