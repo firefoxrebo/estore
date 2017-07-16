734e415092589cebdd85a29348b20945b1e9ff5a
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-16 03:03:00 +0300

    Changed the database name to storedb

--- app/config/config.php
+++ app/config/config.php
@@ -16,7 +16,7 @@ define('JS', '/js/');
 defined('DATABASE_HOST_NAME')       ? null : define ('DATABASE_HOST_NAME', 'localhost');
 defined('DATABASE_USER_NAME')       ? null : define ('DATABASE_USER_NAME', 'root');
 defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', 'rebo');
-defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'mvc');
+defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'storedb');
 defined('DATABASE_PORT_NUMBER')     ? null : define ('DATABASE_PORT_NUMBER', 3306);
 defined('DATABASE_CONN_DRIVER')     ? null : define ('DATABASE_CONN_DRIVER', 1);
 
