b23615f63052ac7a1f812da200a47b4c7d56b571
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-16 03:18:10 +0300

    Modified the default action to get the current users in the database

--- app/controllers/userscontroller.php
+++ app/controllers/userscontroller.php
@@ -1,12 +1,17 @@
 <?php
 namespace PHPMVC\Controllers;
 
+use PHPMVC\Models\UserModel;
+
 class UsersController extends AbstractController
 {
     public function defaultAction()
     {
         $this->_language->load('template.common');
         $this->_language->load('users.default');
+
+        $this->_data['users'] = UserModel::getAll();
+
         $this->_view();
     }
 }
