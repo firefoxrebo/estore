fb8abfaf2e7076787d60f94fc45b7484db1e087b
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-16 03:18:37 +0300

    modified the users.default language file

--- app/languages/ar/users/default.lang.php
+++ app/languages/ar/users/default.lang.php
@@ -2,3 +2,10 @@
 
 $_['title'] = 'المستخدمين و الصلاحيات';
 $_['text_header'] = 'المستخدمين و الصلاحيات';
+$_['text_new_item'] = 'مستخدم جديد';
+$_['text_table_username'] = 'إسم المستخدم';
+$_['text_table_group'] = 'المجموعة التابع لها';
+$_['text_table_email'] = 'البريد الالكتروني';
+$_['text_table_subscription_date'] = 'تاريخ الانضمام';
+$_['text_table_last_login'] = 'اخر وقت دخول';
+$_['text_table_control'] = 'التحكم';
--- app/languages/en/users/default.lang.php
+++ app/languages/en/users/default.lang.php
@@ -2,3 +2,10 @@
 
 $_['title'] = 'Users and Privileges';
 $_['text_header'] = 'Users and Privileges';
+$_['text_new_item'] = 'New User';
+$_['text_table_username'] = 'Username';
+$_['text_table_group'] = 'User Group';
+$_['text_table_email'] = 'Email';
+$_['text_table_subscription_date'] = 'Subscription Date';
+$_['text_table_last_login'] = 'Last Logged In';
+$_['text_table_control'] = 'Control';
