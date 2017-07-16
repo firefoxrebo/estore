84210ff5cb985751a1a399241543900fe1439cc8
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-16 03:02:29 +0300

    Added support for Sessin Manager Class

--- public/index.php
+++ public/index.php
@@ -3,9 +3,8 @@ namespace PHPMVC;
 
 use PHPMVC\LIB\FrontController;
 use PHPMVC\LIB\Language;
+use PHPMVC\LIB\SessionManager;
 use PHPMVC\LIB\Template;
-
-session_start();
 
 if(!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
@@ -13,6 +12,9 @@ if(!defined('DS')) {
 
 require_once '..' . DS . 'app' . DS . 'config' . DS . 'config.php';
 require_once APP_PATH . DS . 'lib' . DS . 'autoload.php';
+
+$session = new SessionManager();
+$session->start();
 
 if(!isset($_SESSION['lang'])) {
     $_SESSION['lang'] = APP_DEFAULT_LANGUAGE;
