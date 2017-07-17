4dd7a7514e07a1dccb6162e12b124d68baeb4dfd
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-17 14:13:21 +0300

    created the Privileges and UsersGroups Controllers

---
+++ app/controllers/privilegescontroller.php
@@ -0,0 +1,86 @@
+<?php
+namespace PHPMVC\Controllers;
+use PHPMVC\LIB\Helper;
+use PHPMVC\LIB\InputFilter;
+use PHPMVC\Models\PrivilegeModel;
+
+class PrivilegesController extends AbstractController
+{
+    use InputFilter;
+    use Helper;
+
+    public function defaultAction()
+    {
+        $this->_language->load('template.common');
+        $this->_language->load('privileges.default');
+
+        $this->_data['privileges'] = PrivilegeModel::getAll();
+
+        $this->_view();
+    }
+
+    // TODO: we need to implement csrf prevention
+    public function createAction()
+    {
+        $this->_language->load('template.common');
+        $this->_language->load('privileges.labels');
+        $this->_language->load('privileges.create');
+
+        if(isset($_POST['submit'])) {
+            $privilege = new PrivilegeModel();
+            $privilege->PrivilegeTitle = $this->filterString($_POST['PrivilegeTitle']);
+            $privilege->Privilege = $this->filterString($_POST['Privilege']);
+            if($privilege->save())
+            {
+                $this->redirect('/privileges');
+            }
+        }
+
+        $this->_view();
+    }
+
+    public function editAction()
+    {
+
+        $id = $this->filterInt($this->_params[0]);
+        $privilege = PrivilegeModel::getByPK($id);
+
+        if($privilege === false) {
+            $this->redirect('/privileges');
+        }
+
+        $this->_data['privilege'] = $privilege;
+
+        $this->_language->load('template.common');
+        $this->_language->load('privileges.labels');
+        $this->_language->load('privileges.edit');
+
+        if(isset($_POST['submit'])) {
+            $privilege->PrivilegeTitle = $this->filterString($_POST['PrivilegeTitle']);
+            $privilege->Privilege = $this->filterString($_POST['Privilege']);
+            if($privilege->save())
+            {
+                $this->redirect('/privileges');
+            }
+        }
+
+        $this->_view();
+    }
+
+    public function deleteAction()
+    {
+
+        $id = $this->filterInt($this->_params[0]);
+        $privilege = PrivilegeModel::getByPK($id);
+
+        if($privilege === false) {
+            $this->redirect('/privileges');
+        }
+
+        if($privilege->delete())
+        {
+            $this->redirect('/privileges');
+        }
+    }
+
+}
--- app/controllers/usersgroupscontroller.php
+++ app/controllers/usersgroupscontroller.php
@@ -1,16 +1,15 @@
 <?php
 namespace PHPMVC\Controllers;
-
-use PHPMVC\Models\UserModel;
+use PHPMVC\Models\UserGroupModel;
 
 class UsersGroupsController extends AbstractController
 {
     public function defaultAction()
     {
         $this->_language->load('template.common');
-        $this->_language->load('users.default');
+        $this->_language->load('usersgroups.default');
 
-        $this->_data['users'] = UserModel::getAll();
+        $this->_data['groups'] = UserGroupModel::getAll();
 
         $this->_view();
     }
