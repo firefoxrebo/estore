efb114aec0f27ed32bc2e958b117f3bae6ead037
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-26 16:40:50 +0300

    added new methods (encrypt password, get all override, userExists)

--- app/models/usermodel.php
+++ app/models/usermodel.php
@@ -3,7 +3,6 @@ namespace PHPMVC\Models;
 
 class UserModel extends AbstractModel
 {
-
     public $UserId;
     public $Username;
     public $Password;
@@ -12,6 +11,7 @@ class UserModel extends AbstractModel
     public $SubscriptionDate;
     public $LastLogin;
     public $GroupId;
+    public $Status;
 
     protected static $tableName = 'app_users';
 
@@ -24,7 +24,29 @@ class UserModel extends AbstractModel
         'SubscriptionDate'  => self::DATA_TYPE_DATE,
         'LastLogin'         => self::DATA_TYPE_STR,
         'GroupId'           => self::DATA_TYPE_INT,
+        'Status'            => self::DATA_TYPE_INT,
     );
 
     protected static $primaryKey = 'UserId';
+
+    public function cryptPassword($password)
+    {
+        $salt = '$2a$07$yeNCSNwRpYopOhv0TrrReP$';
+        $this->Password = crypt($password, $salt);
+    }
+
+    // TODO:: FIX THE TABLE ALIASING
+    public static function getAll()
+    {
+        return self::get(
+        'SELECT au.*, aug.GroupName GroupName FROM ' . self::$tableName . ' au INNER JOIN app_users_groups aug ON aug.GroupId = au.GroupId'
+        );
+    }
+
+    public static function userExists($username)
+    {
+        return self::get('
+            SELECT * FROM ' . self::$tableName . ' WHERE Username = "' . $username . '"
+        ');
+    }
 }
