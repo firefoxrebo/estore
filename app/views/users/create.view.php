abdb043fc878a6f0589a1ebb468f3c96f8050499
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-26 16:41:26 +0300

    modified the create action to use the showValue and labelFloat helpers

--- app/views/users/create.view.php
+++ app/views/users/create.view.php
@@ -2,28 +2,28 @@
     <fieldset>
         <legend><?= $text_legend ?></legend>
         <div class="input_wrapper n40 border">
-            <label><?= $text_label_Username ?></label>
-            <input required type="text" name="Username" maxlength="30">
+            <label<?= $this->labelFloat('Username') ?>><?= $text_label_Username ?></label>
+            <input required type="text" name="Username" maxlength="30" value="<?= $this->showValue('Username') ?>">
         </div>
         <div class="input_wrapper n30 border padding">
-            <label><?= $text_label_Password ?></label>
-            <input required type="password" name="Password">
+            <label<?= $this->labelFloat('Password') ?>><?= $text_label_Password ?></label>
+            <input required type="password" name="Password" value="<?= $this->showValue('Password') ?>">
         </div>
         <div class="input_wrapper n30 padding">
-            <label><?= $text_label_CPassword ?></label>
-            <input required type="password" name="CPassword">
+            <label<?= $this->labelFloat('CPassword') ?>><?= $text_label_CPassword ?></label>
+            <input required type="password" name="CPassword" value="<?= $this->showValue('CPassword') ?>">
         </div>
         <div class="input_wrapper n30 border">
-            <label><?= $text_label_Email ?></label>
-            <input required type="email" name="Email" maxlength="40">
+            <label<?= $this->labelFloat('Email') ?>><?= $text_label_Email ?></label>
+            <input required type="email" name="Email" maxlength="40" value="<?= $this->showValue('Email') ?>">
         </div>
         <div class="input_wrapper n30 border padding">
-            <label><?= $text_label_CEmail ?></label>
-            <input required type="email" name="CEmail" maxlength="40">
+            <label<?= $this->labelFloat('CEmail') ?>><?= $text_label_CEmail ?></label>
+            <input required type="email" name="CEmail" maxlength="40" value="<?= $this->showValue('CEmail') ?>">
         </div>
         <div class="input_wrapper n20 padding border">
-            <label><?= $text_label_PhoneNumber ?></label>
-            <input required type="text" name="PhoneNumber">
+            <label<?= $this->labelFloat('PhoneNumber') ?>><?= $text_label_PhoneNumber ?></label>
+            <input required type="text" name="PhoneNumber" value="<?= $this->showValue('PhoneNumber') ?>">
         </div>
         <div class="input_wrapper_other padding n20 select">
             <select required name="GroupId">
