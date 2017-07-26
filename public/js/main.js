bb4e7060d465a268a7a45066e0c4faee7babe13c
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-26 16:36:41 +0300

    some modifications regarding the user create action

--- public/js/main.js
+++ public/js/main.js
@@ -83,3 +83,38 @@ $('li.submenu > a').click(function()
         $(this).parent().addClass('selected')
     }
 });
+
+(function()
+{
+    var userNameField = document.querySelector('input[name=Username]');
+    if(null !== userNameField) {
+        userNameField.addEventListener('blur', function()
+        {
+            var req = new XMLHttpRequest();
+            req.open('POST', 'http://www.mvcapp.com/users/checkuserexistsajax');
+            req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+            req.onreadystatechange = function()
+            {
+                var iElem = document.createElement('i');
+                if(req.readyState == req.DONE && req.status == 200) {
+                    if(req.response == 1) {
+                        iElem.className = 'fa fa-times error';
+                    } else if(req.response == 2) {
+                        iElem.className = 'fa fa-check success';
+                    }
+                    var iElems = userNameField.parentNode.childNodes;
+                    for ( var i = 0, ii = iElems.length; i < ii; i++ )
+                    {
+                        if(iElems[i].nodeName.toLowerCase() == 'i') {
+                            iElems[i].parentNode.removeChild(iElems[i]);
+                        }
+                    }
+                    userNameField.parentNode.appendChild(iElem);
+                }
+            }
+
+            req.send("Username=" + this.value);
+        }, false);
+    }
+})();
