8e3a89fa2b4a95cc5f6a090bf35511ba0fd08704
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-26 16:37:42 +0300

    added the showValue helper

--- app/lib/template/TemplateHelper.php
+++ app/lib/template/TemplateHelper.php
@@ -7,4 +7,14 @@ trait TemplateHelper
     public function matchUrl($url) {
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === $url;
     }
+
+    public function labelFloat($fieldName)
+    {
+        return isset($_POST[$fieldName]) && !empty($_POST[$fieldName]) ? ' class="floated"' : '';
+    }
+
+    public function showValue($fieldName, $object = null)
+    {
+        return isset($_POST[$fieldName]) ? $_POST[$fieldName] : (is_null($object) ? '' : $object->$fieldName);
+    }
 }
