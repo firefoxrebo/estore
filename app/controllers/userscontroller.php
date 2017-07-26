4f7170c2f54a431641714b21f6868012a589ac39
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-26 16:39:20 +0300

    implemented the create action with validation support

--- app/controllers/userscontroller.php
+++ app/controllers/userscontroller.php
@@ -1,18 +1,24 @@
 <?php
 namespace PHPMVC\Controllers;
 
+use PHPMVC\LIB\Helper;
+use PHPMVC\LIB\InputFilter;
+use PHPMVC\lib\Messenger;
 use PHPMVC\Models\UserGroupModel;
 use PHPMVC\Models\UserModel;
 
 class UsersController extends AbstractController
 {
 
+    use InputFilter;
+    use Helper;
+
     private $_createActionRoles =
     [
         'Username'      => 'req|alphanum|between(3,12)',
-        'Password'      => 'req|min(6)',
+        'Password'      => 'req|min(6)|eq_field(CPassword)',
         'CPassword'     => 'req|min(6)',
-        'Email'         => 'req|email',
+        'Email'         => 'req|email|eq_field(CEmail)',
         'CEmail'        => 'req|email',
         'PhoneNumber'   => 'alphanum|max(15)',
         'GroupId'       => 'req|int'
@@ -30,18 +36,50 @@ class UsersController extends AbstractController
 
     public function createAction()
     {
+
         $this->language->load('template.common');
         $this->language->load('users.create');
         $this->language->load('users.labels');
+        $this->language->load('users.messages');
         $this->language->load('validation.errors');
 
         $this->_data['groups'] = UserGroupModel::getAll();
 
-        if(isset($_POST['submit'])) {
-            $this->isValid($this->_createActionRoles, $_POST);
 
+        if(isset($_POST['submit']) && $this->isValid($this->_createActionRoles, $_POST)) {
+
+            $user = new UserModel();
+            $user->Username = $this->filterString($_POST['Username']);
+            $user->cryptPassword($_POST['Password']);
+            $user->Email = $this->filterString($_POST['Email']);
+            $user->PhoneNumber = $this->filterString($_POST['PhoneNumber']);
+            $user->GroupId = $this->filterInt($_POST['GroupId']);
+            $user->SubscriptionDate = date('Y-m-d');
+            $user->LastLogin = date('Y-m-d H:i:s');
+            $user->Status = 1;
+
+            if($user->save()) {
+                $this->messenger->add($this->language->get('message_create_success'));
+            } else {
+                $this->messenger->add($this->language->get('message_create_failed'), Messenger::APP_MESSAGE_ERROR);
+            }
+            $this->redirect('/users');
         }
 
         $this->_view();
+    }
+
+    // TODO:: make sure this is a AJAX Request
+    // TODO:: explain the different types of headers used in this course
+    public function checkUserExistsAjaxAction()
+    {
+        if(isset($_POST['Username']) && !empty($_POST['Username'])) {
+            header('Content-type: text/plain');
+            if(UserModel::userExists($this->filterString($_POST['Username'])) !== false) {
+                echo 1;
+            } else {
+                echo 2;
+            }
+        }
     }
 }
