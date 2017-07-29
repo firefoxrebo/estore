24c35aff13f7baa4a8faf038a0a6edb9bd2b8bb8
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-29 16:29:19 +0300

    Added authentication property and implemented middleware authentication

--- app/lib/frontcontroller.php
+++ app/lib/frontcontroller.php
@@ -5,6 +5,7 @@ use PHPMVC\LIB\Template\Template;
 
 class FrontController
 {
+    use Helper;
 
     const NOT_FOUND_ACTION = 'notFoundAction';
     const NOT_FOUND_CONTROLLER = 'PHPMVC\Controllers\\NotFoundController';
@@ -15,11 +16,13 @@ class FrontController
 
     private $_registry;
     private $_template;
+    private $_authentication;
 
-    public function __construct(Template $template, Registry $registry)
+    public function __construct(Template $template, Registry $registry, Authentication $auth)
     {
         $this->_template = $template;
         $this->_registry = $registry;
+        $this->_authentication = $auth;
         $this->_parseUrl();
     }
 
@@ -41,6 +44,23 @@ class FrontController
     {
         $controllerClassName = 'PHPMVC\Controllers\\' . ucfirst($this->_controller) . 'Controller';
         $actionName = $this->_action . 'Action';
+
+        // Check if the user is authorized to access the application
+        if(!$this->_authentication->isAuthorized()) {
+            if($this->_controller != 'auth' && $this->_action != 'login') {
+                $this->redirect('/auth/login');
+            }
+        } else {
+            // deny access to the auth/login
+            if($this->_controller == 'auth' && $this->_action == 'login') {
+                isset($_SERVER['HTTP_REFERER']) ? $this->redirect($_SERVER['HTTP_REFERER']) : $this->redirect('/');
+            }
+            // Check if the user has access to specific url
+            if(!$this->_authentication->hasAccess($this->_controller, $this->_action))
+            {
+                $this->redirect('/accessdenied');
+            }
+        }
 
         if(!class_exists($controllerClassName) || !method_exists($controllerClassName, $actionName)) {
             $controllerClassName = self::NOT_FOUND_CONTROLLER;
