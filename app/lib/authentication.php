ce128da8d80877a05472a16cce2a1b8f2d756972
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-29 16:27:30 +0300

    Added AuthenticationController Class

---
+++ app/lib/authentication.php
@@ -0,0 +1,51 @@
+<?php
+namespace PHPMVC\lib;
+
+
+class Authentication
+{
+    private static $_instance;
+    private $_session;
+
+    private $_execludedRoutes = [
+        '/index/default',
+        '/auth/logout',
+        '/users/profile',
+        '/users/changepassword',
+        '/users/settings',
+        '/language/default',
+        '/accessdenied/default',
+        '/notfound/notfound'
+    ];
+
+    private function __construct($session)
+    {
+        $this->_session = $session;
+    }
+
+    private function __clone()
+    {
+
+    }
+
+    public static function getInstance(SessionManager $session)
+    {
+        if(self::$_instance === null) {
+            self::$_instance = new self($session);
+        }
+        return self::$_instance;
+    }
+
+    public function isAuthorized()
+    {
+        return isset($this->_session->u);
+    }
+
+    public function hasAccess($controller, $action)
+    {
+        $url = strtolower('/' . $controller . '/' . $action);
+        if(in_array($url, $this->_execludedRoutes) || in_array($url, $this->_session->u->privileges)) {
+            return true;
+        }
+    }
+}
