e4bb9bfc8c31d485b633a0799bd9d4aa3af859df
Mohammed Yehia <m.yehia@dev-gate.com>
2017-07-29 16:10:35 +0300

    Added Authentication Login Action View and come related images regarding the login form

---
+++ app/views/auth/login.view.php
@@ -0,0 +1,19 @@
+<div class="action_view login">
+    <?php $messages = $this->messenger->getMessages(); if(!empty($messages)): foreach ($messages as $message): ?>
+        <p class="message t<?= $message[1] ?>"><?= $message[0] ?><a href="" class="closeBtn"><i class="fa fa-times"></i></a></p>
+    <?php endforeach;endif; ?>
+    <div class="login_box">
+        <form autocomplete="off" method="post" enctype="application/x-www-form-urlencoded">
+            <div class="border"></div>
+            <h1><?= $login_header ?></h1>
+            <img src="/img/login-icon.png" width="120">
+            <div class="input_wrapper username">
+                <input required type="text" name="ucname" id="ucname" maxlength="50" placeholder="<?= $login_ucname ?>">
+            </div>
+            <div class="input_wrapper password">
+                <input required type="password" id="ucpwd" name="ucpwd" maxlength="100" placeholder="<?= $login_ucpwd ?>">
+            </div>
+            <input type="submit" name="login" value="<?= $login_button ?>">
+        </form>
+    </div>
+</div>
